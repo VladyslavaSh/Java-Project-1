baeed156122313c77fda3edd129ca7757db1c948
Vladyslava Shekula <shekulavlada@gmail.com>
2022-02-15 17:29:13 +0200

    Finish task 1

--- src/main/java/ee/taltech/b_practise/assignment1/Subtraction.java
+++ src/main/java/ee/taltech/b_practise/assignment1/Subtraction.java
@@ -6,12 +6,21 @@ public class Subtraction {
     // and returns the result of subtracting second element from the first
     // P.S pay attention to function and parameter names
 
+    public static int function(int a, int b){
+
+        return b - a;
+    }
+
+
     public static void main(String[] args) {
         //todo B
         // - call your newly created function with 5 and 2, print out the result
         // - call your newly created function with -5 and -2, print out the result
 
-
+        int result1 = function(5, 2);
+        System.out.println(result1);
+        int result2 = function(-5, -2);
+        System.out.println(result2);
 
         //todo C if you were to add another method
         // with the same name, but 3 inputs, then it would be an example of?
@@ -20,7 +29,7 @@ public class Subtraction {
         // c) overriding?
         // d) overreacting?
         // e) overlooking?
-        // Answer:
+        // Answer: с
     }
 }
 
