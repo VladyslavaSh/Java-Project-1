f07569b56673bc81dd712c484d0a2fa2d3e86d75
mgoroz <mgoroz@ttu.ee>
2022-02-16 21:28:22 +0000

    complete assignment3

--- src/main/java/ee/taltech/b_practise/assignment3/Multiplier.java
+++ src/main/java/ee/taltech/b_practise/assignment3/Multiplier.java
@@ -1,10 +1,13 @@
 package ee.taltech.b_practise.assignment3;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Multiplier {
-
     //todo A modify function to pass tests
+    public static int function(int a, int b){
+        return a * b;
+    }
     //todo B modify function to pass edge case tests
     //todo C make your solution pretty
     //todo D use streams
@@ -13,6 +16,19 @@ public class Multiplier {
      * multiplies each param element with multiplier
      */
     public static List<Integer> multiplier(List<Integer> params, Integer multiplier) {
-        return params;
+        if (params == null){
+            return List.of();
+        }
+        List<Integer> l = new ArrayList<>(params);
+        if (multiplier == null){
+            return params;
+        }
+        else{
+            for (int i = 0; i < l.size(); i++){
+                int a = l.get(i);
+                l.set(i, a * multiplier);
+            }
+        }
+        return l;
     }
 }
