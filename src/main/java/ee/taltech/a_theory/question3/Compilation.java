24aa9c4ab426c0214af0ef8daf17f5c5efeccb97
Maksim Gorozhanko <Polotenchik10>
2022-02-19 20:34:40 +0200

    theory 3 completed, pseudocode2 upd

--- src/main/java/ee/taltech/a_theory/question3/Compilation.java
+++ src/main/java/ee/taltech/a_theory/question3/Compilation.java
@@ -5,30 +5,30 @@ public class Compilation {
     //todo A
     // in your own words (do not use wiki definitions)
     // What is compilation?
-    // Answer:
-    //
+    // Answer: Compilation is process of translation of source code
+    // to a machine code or lower level programming language
     //
     //
     //
     //todo B
     // Name 2 benefits of compilation?
     // Benefits:
-    // 1
-    // 2
+    // 1 Compilation makes it easier to control memory management
+    // 2 Compilation often leads to higher efficiency
     //todo C
     // Name 1 disadvantage of compilation?
     // Disadvantage:
-    // 1
+    // 1 Programs need to be built after each change
     //todo D
     // Name 2x other languages using compiler
-    // 1
-    // 2
+    // 1 C++
+    // 2 Haskell
     //todo E
     // What is opposite of compiled language?
-    // Answer: _____ language (1 word)
+    // Answer: interpreted language (1 word)
     //todo F
     // Name 2x languages of E
-    // 1
-    // 2
+    // 1 Python
+    // 2 Ruby
 
 }
